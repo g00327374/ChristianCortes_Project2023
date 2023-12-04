c22af2425a4ea78d25434e50ade3ab4a311ef0a1
g00327374 <g00327374@atu.ie>
2023-12-04 14:45:15 +0000

    new commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -165,7 +165,7 @@ app.get('/test', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-//add at the bottom just over app.listen
+// add at the bottom just over app.listen
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../build/index.html'));
